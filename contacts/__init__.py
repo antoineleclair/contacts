af88a380985c74a83d37d696f466e3f4c1611ed9
Antoine Leclair <antoineleclair@gmail.com>
2012-02-18 21:58:13 -0500

    Adds mongo.

--- contacts/__init__.py
+++ contacts/__init__.py
@@ -1,10 +1,24 @@
 from pyramid.config import Configurator
+from pyramid.events import subscriber
+from pyramid.events import NewRequest
+
+import pymongo
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    
+    conn = pymongo.Connection(settings['mongo.uri'])
+    config.registry.settings['mongo_connection'] = conn
+    config.add_subscriber(add_mongo_db, NewRequest)
+    
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
+    
+def add_mongo_db(event):
+    settings = event.request.registry.settings
+    db = settings['mongo_connection'][settings['mongo.db']]
+    event.request.db = db
